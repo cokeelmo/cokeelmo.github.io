042405442f4f2b9e49d2a1bb927e2d61bccdcc04
TibePrincenPXL <TibePrincen@student.pxl.be>
2025-04-08 15:30:38 +0200

    br br patapim

--- animate.js
+++ animate.js
@@ -23,3 +23,34 @@ window.onmousemove = e => {
         image.animate({objectPosition: `${100 + nextpercentage}% center`}, {duration: 1200, fill: "forwards"});
     }
 }
+
+let touchStartX = 0;
+
+track.addEventListener("touchstart", (e) => {
+  touchStartX = e.touches[0].clientX;
+  track.dataset.mouseDownAt = touchStartX;
+});
+
+track.addEventListener("touchend", () => {
+  track.dataset.mouseDownAt = "0";
+  track.dataset.prevPercentage = track.dataset.percentage;
+});
+
+track.addEventListener("touchmove", (e) => {
+  if (track.dataset.mouseDownAt === "0") return;
+
+  const touchDelta = parseFloat(track.dataset.mouseDownAt) - e.touches[0].clientX;
+  const maxDelta = window.innerWidth / 2;
+
+  let percentage = (touchDelta / maxDelta) * -100;
+  let nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+  nextPercentage = Math.max(Math.min(nextPercentage, 0), -100);
+
+  track.dataset.percentage = nextPercentage;
+
+  track.style.transform = `translate(${nextPercentage}%, -50%)`;
+
+  for (const image of track.getElementsByClassName("images")) {
+    image.style.objectPosition = `${100 + nextPercentage}% center`;
+  }
+});
